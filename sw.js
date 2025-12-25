24b0901263b88923a0c8a355b001a07b8ffce506
Anakin-new <gucciom4@gmail.com>
2025-12-25 21:32:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,27 @@
 const CACHE_NAME = 'sayli-garden-v1';
 
-// List every file needed to make the app run offline
 const ASSETS = [
     './dashboard.html',
+    './index.html',
     './syllabus.html',
     './history.html',
     './library.html',
     './notes.html',
     './style-sheets/dashboard.css',
-    './style-sheets/syllabus.css',
+    './style-sheets/auth.css',
+    './style-sheets/hisory.css',
     './style-sheets/library.css',
     './style-sheets/notes.css',
+    './style-sheets/syllabus.css',
     './js/dashboard.js',
+    './js/auth.js',
     './js/library.js',
     './js/syllabus-script.js',
     './assets/new.mp4',
-    './manifest.json',
-    './assets/icon-192.png',
-    './assets/icon-512.png'
+    './assets/icon.png',
+    './manifest.json'
 ];
 
-// Install Event
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
@@ -29,7 +30,6 @@ self.addEventListener('install', (event) => {
     );
 });
 
-// Fetch Event
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
